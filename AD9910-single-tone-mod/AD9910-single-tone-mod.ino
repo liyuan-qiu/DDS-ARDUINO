a981eeade6dd1beeaa20b21cd822b629eda932e1
liyuan-qiu <39110126+liyuan-qiu@users.noreply.github.com>
2021-09-18 12:17:46 +0800

    Add files via upload

---
+++ AD9910-single-tone-mod/AD9910-single-tone-mod.ino
@@ -0,0 +1,138 @@
+#include <Arduino.h>
+
+const int SPI_SCLK =13;
+const int SPI_SDIO = 11;
+
+const int OSK = 40;
+const int PR0[4] = {4,4,4,4};
+const int PR1[4] = {3,3,3,3};
+const int PR2[4] = {2,2,2,2};
+
+const int SPI_CSB[4] = {10,10,10,10};
+//Chip Select. Digital input (active low). This pin allows 
+//the AD9910 to operate on a common serial bus for the control 
+//data path. Bringing this pin low enables the AD9910 to detect 
+//serial clock rising/falling edges. Bringing this pin high 
+//causes the AD9910 to ignore input on the serial data pins.
+const int MASTER_RESET[4] = {A3,A3,A3,A3};
+//Master Reset, Digital Input (Active High). 
+//Master reset: clears all memory elements and sets registers to default values.
+
+const int IO_UPDATE[4] = {6,6,6,6};
+//Input/Output Update. Digital input (active high). 
+//A high on this pin transfers the contents of the 
+//I/O buffers to the corresponding internal registers.
+/*const int SPI_CSB = 9;
+const int MASTER_RESET = 23;
+const int PR0 = 29;
+const int PR1 = 30;
+const int IO_UPDATE = 2;*/
+
+/*const int SPI_CSB = 8;
+const int MASTER_RESET = 7;
+const int PR0 = 6;
+const int PR1 = 20;
+const int IO_UPDATE = 21;*/
+
+/*const int SPI_CSB = 26;
+const int MASTER_RESET = 31;
+const int PR0 = 4;
+const int PR1 = 3;
+const int IO_UPDATE = 24;*/
+
+void write1_writen(int slave, int instruction, int n, byte* data) {//instruction:address ,data: what you want to write
+      digitalWrite(SPI_SCLK, LOW); // ensure clock pin LOW when we start
+      digitalWrite(SPI_CSB[slave], LOW);
+      
+      shiftOut(SPI_SDIO, SPI_SCLK, MSBFIRST, instruction);
+      for (int i = 0; i < n; ++i) shiftOut(SPI_SDIO, SPI_SCLK, MSBFIRST, data[i]);
+      
+      digitalWrite(SPI_CSB[slave], HIGH);
+};
+
+void write1_readn(int slave, int instruction, int n, byte *data) {
+      digitalWrite(SPI_SCLK, LOW); // ensure clock pin LOW when we start
+      digitalWrite(SPI_CSB[slave], LOW);
+      //shiftOut(SPI_SDIO, SPI_SCLK, MSBFIRST, (instruction >> 8)); // high byte
+      shiftOut(SPI_SDIO, SPI_SCLK, MSBFIRST, instruction); // low byte
+      
+      pinMode(SPI_SDIO, INPUT);
+      for (int i = 0; i < n; ++i) data[i] = shiftIn(SPI_SDIO, SPI_SCLK, MSBFIRST);
+      
+      digitalWrite(SPI_CSB[slave], HIGH);
+      pinMode(SPI_SDIO, OUTPUT);  
+};
+static void update(int slave) {
+    digitalWrite(IO_UPDATE[slave], HIGH);
+    delay(1);
+    digitalWrite(IO_UPDATE[slave], LOW);
+}
+
+void setup() {
+    Serial.begin(9600);
+
+    pinMode(OSK,OUTPUT);
+    //pinMode(PR2,OUTPUT);
+    //digitalWrite(PR2,LOW);
+    pinMode(PR2,INPUT);
+    pinMode(SPI_SCLK, OUTPUT);
+    pinMode(SPI_SDIO, OUTPUT);
+
+    digitalWrite(OSK,HIGH);
+    digitalWrite(SPI_SCLK, LOW);
+      
+    for (int i = 0; i < 4; ++i) {
+      pinMode(MASTER_RESET[i],OUTPUT);
+      pinMode(SPI_CSB[i],OUTPUT);
+      pinMode(IO_UPDATE[i],OUTPUT);
+      //pinMode(PR0[i],OUTPUT);
+      //pinMode(PR1[i],OUTPUT);
+      //digitalWrite(PR0[i],LOW);
+      //digitalWrite(PR1[i],LOW);
+      pinMode(PR0[i],INPUT);
+      pinMode(PR1[i],INPUT);
+
+      digitalWrite(MASTER_RESET[i],HIGH);
+      delay(1);
+      digitalWrite(MASTER_RESET[i],LOW);// this master reset from high to low, reset to default setting of the device
+      delay(1);
+  
+      digitalWrite(SPI_CSB[i],HIGH); // deactivate
+      digitalWrite(IO_UPDATE[i],LOW);
+    } 
+}
+
+byte state = 0, slave = 0, len = 0, pos = 0, data[9];
+void loop() {  
+  while (Serial.available()) {
+    byte b = Serial.read();
+    Serial.write(b);//这边回给电脑一个重复的命令
+    
+    if (state == 0) {
+      slave = (b & 0x60) >> 5;//对应matlab里面32*i，判断是第几个slave，这位运算张翔写的绝了，太厉害了
+      len = b & 0x1F;//对应matlab里面32*i+4/8/128/136,4的话对应的事寄存器write，8的话对应的是数据write，128的话对应的是update，136的话对应的是read，因为有三种状态，write/update/read，所以需要两个判断，包括（b & 0x80）来判断是update/read还是write，然后第二个（b & 0x1F）来区分是update或者read
+      if (b & 0x80)  {//判断2进制第一位是否为1，即>=128 or1000 0000or 0x80    read or update      
+        //如果update b=0x80,len=0,如果 read b>80,通常是84 或 88，所以 len=4或者8
+        if (len == 0)//判断b如果等于128
+          update(slave);
+        else //如果b大于128， read
+          state = 1;
+      } else  { // 如果b<128 write,此处是数据位数
+        state = 2;
+        ++len;
+        pos = 0;
+      }
+    } else if (state == 1) {
+      write1_readn(slave, b | 0x80, len, data);//此时b为传过来的第二位，即地址，b|80表示地址位字节的第一位设为1，后面的bit照抄地址位,详见说明书P48，表示读操作和地址位
+      for (int i = 0; i < len; ++i)//读取信息
+        Serial.write(data[i]);
+      state = 0;
+    } else if (state == 2) {
+       data[pos] = b;//这样写可以保证data[9]都可以读到b的数据吗，有点不懂？？？
+       if (++pos == len) {
+          write1_writen(slave,data[0],len-1,data+1);//???为什么最后的data要+1
+          state = 0;
+       }
+    }
+  }
+}
---
+++ AD9910-single-tone-mod/single_dds_9910.m
@@ -0,0 +1,146 @@
+% %% 通讯协议
+% %输入字符串，字节位
+% % byte1 update 128 len = b & 0x1F==0
+% %       read   >128,=128+数据长度，b & 0x80第一位为1，同时len = b & 0x1F!=0
+% %       write  数据长度，通常是4或8, b & 0x80第一位为0
+% %同时byte1的最后五位2进制是用来确定选择哪一个slave片的，用32*i中的i=0,1,2,3来判断
+% % byte2 寄存器地址 0~32，常用地址14 （0x0E），是波形参数寄存器
+% % byte3~ 数据 
+% %      data byte 1 2| 3 4| 5 6 7 8
+% %      data byte 1~2 幅值参数14位，最大2^14对应幅值幅值5V，峰峰值10V，输入数据对应（byte1*128+byte2）/2^14 *5V
+% %      data byte 3~4 相位参数16位
+% %      data byte 5~8 频率参数32位，最大模拟量1GHz，实际模拟输出1GHz*（byte5*256^3+byte6*256^2+byte7*256+byte8)/2^32
+% 
+% 
+% % Instrument Connection
+% 
+% % Find a serial port object.
+% com = instrfind('Type', 'serial', 'Port', 'COM10', 'Tag', '');
+% 
+% % Create the serial port object if it does not exist
+% % otherwise use the object that was found.
+% if isempty(com)
+%     com = serial('COM10');
+% else
+%     fclose(com);
+%     com = com(1);
+% end
+% 
+% % Connect to instrument object, obj1.
+% fopen(com); %如果此时串口被其他程序占用，会报错，需要重启单片机
+% pause(3);
+% % % %========================================================
+reference_CLK=50*10^6;
+SYS_CLK=10^9;
+PLL_multiple=round(SYS_CLK/reference_CLK);
+frequency_set=10*10^6;%设定的frequency大小
+AMP_Max=1;%这里需要再弄成dBm为单位的设定
+phase_set=0;
+AMP_Set=0.5;
+i=0
+%AD9910 initialization
+%第一位 update 128；write <=9;read >128,数据位数等于第一位-128
+fwrite(com,[32*i+4,0,0,128,0,0],'uint8');%第一位空：写；第二位4：写4位；第三位0：寄存器地址0；剩下的是写入的数据
+fwrite(com,[32*i+4,1,1,64,8,32],'uint8');%寄存器地址1
+fwrite(com,[32*i+4,2,29,63,65,PLL_multiple],'uint8');%寄存器地址2(CFR2)，这个是确定PLL的倍频倍数的,即PLL_multiple
+fwrite(com,[32*i+4,3,0,0,0,127],'uint8');%寄存器地址3
+
+%写入波形，8位数据，地址E
+% if i==0
+% fwrite(com,[32*i+8,14,23,0,0,0,64,0,0,0],'uint8');%数据长度8，寄存器地址E，幅值,相位，控制频率
+% else
+% fwrite(com,[32*i+8,14,63,255,0,0,66,143,92,40],'uint8');
+% end
+
+% 15.625MHz, ~980mV
+[freq_B1,freq_B2,freq_B3,freq_B4]=encodeFreq(frequency_set,SYS_CLK);
+[amp_B1,amp_B2]=encodeAmp(AMP_Set,AMP_Max);
+[phase_B1, phase_B2]=encodePhase(phase_set, max_phase);
+fwrite(com,[32*i+8,14,amp_B1,amp_B2,phase_B1,phase_B2,freq_B1,freq_B2,freq_B3,freq_B4],'uint8');%数据长度8，寄存器地址E，幅值,相位，控制频率fwrite(com,[32*i+8,14,63,255,0,0,66,143,92,40],'uint8');
+
+
+% fwrite(com,[32*i+8,15,63,255,0,0,2,0,0,0],'uint8');
+% fwrite(com,[32*i+8,16,63,255,0,0,3,0,0,0],'uint8');
+% fwrite(com,[32*i+8,17,63,255,0,0,4,0,0,0],'uint8');
+% fwrite(com,[32*i+8,18,63,255,0,0,5,0,0,0],'uint8');
+% fwrite(com,[32*i+8,19,63,255,0,0,6,0,0,0],'uint8');
+% fwrite(com,[32*i+8,20,63,255,0,0,7,0,0,0],'uint8');
+% fwrite(com,[32*i+8,21,63,255,0,0,64,0,0,0],'uint8');
+fwrite(com,[32*i+128],'uint8');%update
+
+% read，数据长度=136-128=8位，14：地址E，读出频率相位幅值数据
+%fwrite(com,[32*i+136,14],'uint8');
+%
+%    63-幅值 14位
+%    255-
+
+%      0-相位，16位
+%      0-
+
+%      1 -
+%      1 频率，15.计算方法是，AD9910频率是32位寄存器，2^32
+%      对应1GHz,输入数据每一位代表一个字节，即2进制的8位，最大值是2^8，所以数值是1*2^0+1*2^8+1*2^16+1*2^24=freq,实际输出频率=1GHZ*freq/2^32,此处是3.9MHz
+%      1
+%      1 -
+
+% update
+%out = fread(com,8,'uint8')
+function [freq_B1,freq_B2,freq_B3,freq_B4]=encodeFreq(frequency_set,SYS_CLK)
+
+%         # encode a frequency value in MHz from float to 4 bytes uint8 (32bits)
+%         # DDS frequency range 0 - 1GHz (default)
+%         # actual frequency value is:
+%         #   1GHz *（ byte1 * 256^3 + byte2 * 256^2 + byte3 * 256 + byte4 ) / 2^32  
+%         # max frequency bytes are 255 255 255 255
+    freq_i=min(round(frequency_set/SYS_CLK*2^32),2^32-1);
+    freq_B1=floor(freq_i / 2^24); 
+    freq_i = freq_i - freq_B1 * 2^24;
+    freq_B2 = floor(freq_i / 2^16);
+    freq_i = freq_i - freq_B2 * 2^16;
+    freq_B3 = floor(freq_i / 2^8);
+    freq_i = freq_i - freq_B3 * 2^8;
+    freq_B4 = floor(freq_i);
+
+end
+function [freq_B1,freq_B2,freq_B3,freq_B4]=encodeFreq2(frequency_set,SYS_CLK)
+    reference_CLK=50*10^6;
+    SYS_CLK=10^9;
+    PLL_multiple=round(SYS_CLK/reference_CLK);
+    frequency_set=10*10^6;%设定的frequency大小
+    freq_i=uint32(min(round(frequency_set/SYS_CLK*2^32),2^32-1));
+    freq_B1=bitshift(freq_i,-24);
+    freq_i=uint32(bitshift(freq_i,8));
+    freq_B2=bitshift(freq_i,-24);
+    freq_i=uint32(bitshift(freq_i,8));
+    freq_B3=bitshift(freq_i,-24); 
+    freq_i=uint32(bitshift(freq_i,8));
+    freq_B4=bitshift(freq_i,-24); 
+end
+function [amp_B1,amp_B2]=encodeAmp(AMP_Set,AMP_Max)
+%         # encode a amplitude value from float to 2 bytes uint8 (14bits)
+%         # DDS amplitude range 0 - 1 (default), the max amplitude of each DDS at different frequencies are differnet, so we use a unit value max_amp to represent them
+%         # actual amplitude value is:
+%         #   max_amp *（ byte1 * 256 + byte2 ) / 2^14
+%         # max amplitude bytes are 63 255
+        amp = min(AMP_Set, AMP_Max);
+        amp_i = min(floor(amp / max_amp * 2^14) , 2^14-1);
+        amp_B1 = floor(amp_i / 2^8);
+        amp_i = amp_i - amp_B1 * 2^8;
+        amp_B2 = floor(amp_i);
+end
+function [phase_B1, phase_B2]=encodePhase(phase_set, max_phase)
+%     def __encodePhase(self,phase, max_phase = 360):
+%         # encode a phase value in degree from float to 2 bytes uint8 (16bits)
+%         # DDS phase range 0-360 degree
+%         # actual Amplitude value is:
+%         #   360 *（ byte1 * 256 + byte4 ) / 2^16
+%         # max phase bytes are 255 255
+    max_phase=360
+    phase_i = min(floor(phase / max_phase * 2^16) , 2^16-1);
+    phase_B1 = floor(phase_i / 2^8);
+    phase_i = phase_i - phase_B1 * 2^8;
+    phase_B2 = floor(phase_i);
+
+end
+      
+% fclose(com);
