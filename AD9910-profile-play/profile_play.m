3f4920a25794bdefb82d3d223b7617314167ea23
liyuan-qiu <39110126+liyuan-qiu@users.noreply.github.com>
2021-09-23 18:21:10 +0800

    Update profile_play.m

--- AD9910-profile-play/profile_play.m
+++ AD9910-profile-play/profile_play.m
@@ -1,16 +1,26 @@
 global com;
-freq_set=[300,300,300,300,300,300,300,300];
-amp_set=(-35:5:-0);
+freq_set=(100:50:500);%MHz
+amp_set=[-30,-15,-30,-15,-30,-15,-30,-15];%dBm
 Profile_set=[0,1,2,3,4,5,6,7];
-for count1=1:length(freq_set)
-    load_PROF_out(freq_set(count1)*10^6,amp_set(count1),Profile_set(count1));
-    pause(1)
+
+time_set=ones(1,12)*1000*1000;%delay us
+profile_list=[0,1,2,3,4,3,6,4,3,2];
+time_length=min([length(profile_list),length(time_set)]);
+if (sum(Profile_set>7)|sum(Profile_set<0)|sum(profile_list>7)|sum(profile_list<0))
+    print('Profile should be 0,1,2....7')
+else
+    for count1=1:min([length(freq_set),length(freq_set),length(Profile_set)],8)
+        load_PROF_out(freq_set(count1)*10^6,amp_set(count1),Profile_set(count1));
+        pause(1)
+    end
+% time_set=[1,2,3,4,5,6,7]*50;%delay us
+
+    request1=['Profile Play ',num2str(time_length,'%02d'),'group']
+    for count2=1:time_length
+        request1=[request1,num2str(time_set(count2),'%010d'),num2str(profile_list(count2),'%01d')];
+        
+    end
+    request1
+    fprintf(com,'%s\n',request1)
 end
-time_set=(0.1:0.1:0.8)*1000;%delay us
-time_length=length(time_set);
-request1=['Profile Play ',num2str(time_length,'%02d'),'group']
-for count2=1:length(time_set)
-    request1=[request1,num2str(time_set(count2),'%06d')];
-end
-request1
-fprintf(com,'%s\n',request1)
+
