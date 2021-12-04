463c9b074984c6434734761680494e2de1075900
liyuan-qiu <39110126+liyuan-qiu@users.noreply.github.com>
2021-12-04 18:31:23 +0800

    Update single_tone_out.m

--- AD9910_with_trigger_interrupt/single_tone_out.m
+++ AD9910_with_trigger_interrupt/single_tone_out.m
@@ -1,6 +1,6 @@
 function single_tone_out(MHz,kHz,Hz,Amp_dB)
 global com;
-request1=['Single Tone ',num2str(MHz,'%03d'),'MHz',num2str(kHz,'%03d'),'kHz',num2str(Hz,'%03d'),'HHz', num2str(-1*Amp_dB,'%03d'),'dBm']
+request1=['Single Tone ',num2str(MHz,'%03d'),'MHz',num2str(kHz,'%03d'),'kHz',num2str(Hz,'%03d'),'HHz', num2str(-1*Amp_dB,'%03d'),'dBm',num2str(Phase,'%03d'),'Deg']
 % request1=strcat("Single Tone ", num2str(-1*Amp_dB),'dBm',num2str(MHz),'MHz',num2str(kHz),'kHz',num2str(Hz),'HHz')
 
 if ((MHz*10^6 + kHz*10^3 + Hz)>600*10^6)
